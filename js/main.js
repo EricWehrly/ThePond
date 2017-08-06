8d8e525b4c1b5dfbdc629d5d6a25e89da3c027f1
EricWehrly <eric@disruptiontheory.com>
2017-08-06 16:52:29 -0400

    sort functionality

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@
 // https://www.w3schools.com/w3css/w3css_cards.asp
 // http://www.unicode.org/emoji/charts/full-emoji-list.html
 
+var LocalUser = {};
 var currentTask = 0;
 
 jQuery(function() {
+
+    var ul = document.querySelector('.sortable');
+    for (var i = ul.children.length; i >= 0; i--) {
+        ul.appendChild(ul.children[Math.random() * i | 0]);
+    }
+
+    jQuery( ".sortable" ).sortable({
+        placeholder: "ui-state-highlight"
+    }).disableSelection();
+
+    // for sorted lists, hide all but the first three
+    jQuery(".sortable .panel:gt(2)").hide();
 
     showTask();
 });
@@ -19,7 +32,7 @@ function showTask(taskNumber) {
     if(jQuery(".task-parent:visible").length == 0) {
         jQuery(jQuery(".task-parent")[taskNumber]).fadeIn(500);
     } else {
-        jQuery(".task-parent:visible").fadeOut(500, function() {
+        jQuery(".task-parent:visible").fadeOut(750, function() {
             jQuery(jQuery(".task-parent")[taskNumber]).fadeIn(500);
         });
     }
@@ -42,7 +55,9 @@ function shakeDeny(element, message) {
 
 function genderPreference(preference) {
 
-    console.log(preference);
+    LocalUser.GenderPreference = preference;
 
-    
+    // jQuery(".task-parent .panel-body:not(." + preference + ")").parent().fadeOut(500, function() {
+        nextTask();
+    // });
 }
