4a2a0252b9fd6922b67459014d0888f8fc200d49
EricWehrly <eric@disruptiontheory.com>
2017-08-06 12:48:01 -0400

    transition between tasks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 
+// https://jsfiddle.net/ramnathv/1064q7jm/
+// https://www.w3schools.com/w3css/w3css_cards.asp
+
+var currentTask = 0;
+
+jQuery(function() {
+
+    showTask();
+});
+
+function showTask(taskNumber) {
+
+    // check if task number exists?
+
+    if(taskNumber == null) taskNumber = currentTask;
+
+    if(jQuery(".task-parent:visible").length == 0) {
+        jQuery(jQuery(".task-parent")[taskNumber]).fadeIn(500);
+    } else {
+        jQuery(".task-parent:visible").fadeOut(500, function() {
+            jQuery(jQuery(".task-parent")[taskNumber]).fadeIn(500);
+        });
+    }
+}
+
+function nextTask() {
+
+    currentTask++;
+
+    showTask();
+}
+
+function shakeDeny(element, message) {
+
+    // shake the element
+
+    // show a message, in red, at the bottom of the page 
+        // (explaining the error)
+}
